7c0e86282832acaa4e312f332c3c345ce6967831
Peanut888 <justinlovesborja5678@gmail.com>
2016-03-07 23:16:11 -0800

    Added files via upload

---
+++ Wireless_Experiment_3_RX/Wireless_Experiment_3_RX.ino
@@ -0,0 +1,44 @@
+/*
+ * Justin Lam
+ * Mars Rover Program
+ * Wireless Experiment 3 RX
+ * 05/02/16
+ */
+
+#include <Servo.h>
+#include <EasyTransfer.h>
+
+EasyTransfer ET;
+
+int servoPan = 5;
+int servoTilt = 6;
+
+struct RECIEVE_DATA_STRUCTURE{
+  int degree1;
+  int degree2;
+};
+
+RECIEVE_DATA_STRUCTURE txdata;
+
+Servo servo1;
+Servo servo2;
+
+void setup(){
+  servo1.attach(servoPan);
+  servo2.attach(servoTilt);
+
+  Serial.begin(9600);
+  
+  ET.begin(details(txdata), &Serial);
+  
+  servo1.write(90);
+  servo2.write(90);
+}
+
+void loop(){
+  if(ET.receiveData()){
+    servo1.write(txdata.degree1);
+    servo2.write(txdata.degree2);
+  }
+}
+
---
+++ Wireless_Experiment_3_TX/Wireless_Experiment_3_TX.ino
@@ -0,0 +1,53 @@
+/*
+ * Justin Lam
+ * Mars Rover Program
+ * Wireless Esperiment 3 TX
+ * 05/02/16
+ */
+#include <EasyTransfer.h>
+
+EasyTransfer ET;
+
+int joyX = A0; // Attach x-axis of joystick to Analog Pin 0.
+int joyY = A1; // Attach y-axis of joystick to Analog Pin 1.
+
+int val1 = 0;
+int val2 = 0;
+int read1 = 0;
+int read2 = 0;
+
+struct SEND_DATA_STRUCTURE{
+  int degree1;
+  int degree2;
+};
+
+SEND_DATA_STRUCTURE txdata;
+
+void setup(){
+  pinMode(joyX, INPUT); // Set x-axis of joystick as an input.
+  pinMode(joyY, INPUT); // Set y-axis of joystick as an input.
+
+  ET.begin(details(txdata), &Serial);
+
+  Serial.begin(9600); // Initialise serial communication.
+}
+
+void loop(){
+  // Pan
+  read1 = analogRead(joyX); // Read the input from the x-axis of the joystick.
+  val1 = map(read1, 0, 1032, 0, 180); // Scale the values
+  txdata.degree1 = val1; // Send values over serial.
+  delay(100); 
+
+  // Tilt 
+  read2 = analogRead(joyY); // Read the input from the y-axis of the joystick.
+  val2 = map(read2, 0, 1032, 0, 180); // Scale the value
+  txdata.degree2 = val2 ; // Send values over serial.
+  delay(100);
+
+  ET.sendData();
+}
+
+
+
+
